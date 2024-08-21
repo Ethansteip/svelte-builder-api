e7616741800111dc7fa865e7425e12f5ed7f6e2a
Ethan <ethan.steip@gmail.com>
2024-08-20 22:02:55 -0400

    testing out running docker containers from express

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import Docker from 'dockerode';
 
 export const app = express();
+
+const docker = new Docker();
+const containerName = 'sveltekit-app-container';
 
 app.use(cors({ origin: true }));
 
@@ -18,6 +22,39 @@ const api = express.Router();
 
 api.get('/hello', (req, res) => {
   res.status(200).send({ message: 'hello world' });
+});
+
+api.post('/start-container', async (req, res) => {
+  try {
+    console.log('incoming request!');
+    // check if the container already exists
+    let container = await docker.listContainers({
+      all: true,
+      filters: { name: [containerName] }
+    });
+
+    console.log('Container: ', container);
+
+    if (!container.length) {
+      container = await docker.createContainer({
+        Image: 'coder-3d2cd7db-d4e2-4a7d-b0b2-b4e6182c1472:latest',
+        name: containerName,
+        ExposedPorts: { '3000/tcp': {} },
+        HostConfig: {
+          PortBindings: { '3000/tcp': [{ HostPort: '3000' }] }
+        }
+      });
+
+      // If the container exists but is stopped, get its instance
+      // container = docker.getContainer(container[0].id);
+
+      await container.start();
+      res.status(200).send('Container started succesfully');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Failed to start container');
+  }
 });
 
 // Version the api
--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,4 @@ import { app } from './api';
 
 const port = process.env.PORT || 3333;
 
-app.listen(port, () =>
-  console.log(`API available on http://localhost:${port}`)
-);
+app.listen(port, () => console.log(`API available on [::]${port}`));
