8647cef4b7115d79fbdabc29ffad98a1f52ac159
Ethan <ethan.steip@gmail.com>
2024-12-13 18:33:28 -0500

    updating env vars

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 import dotenv from 'dotenv';
-
+// comments
 dotenv.config();
 
 if (!process.env.SUPABASE_URL || !process.env.SUPABASE_SERVICE_ROLE_KEY) {
