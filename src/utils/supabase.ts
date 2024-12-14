9715e8d23e33e462636d5db428c930cb9dadada9
Ethan <ethan.steip@gmail.com>
2024-12-13 19:01:26 -0500

    adding env vars

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,19 +1,15 @@
 import { createClient } from '@supabase/supabase-js';
-import { config } from 'dotenv';
-// comments
-config();
 
-if (!process.env.SUPABASE_URL || !process.env.SUPABASE_SERVICE_ROLE_KEY) {
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+if (!supabaseUrl || !supabaseServiceRoleKey) {
   throw new Error('Missing Supabase environment variables');
 }
 
-export const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY,
-  {
-    auth: {
-      autoRefreshToken: false,
-      persistSession: false
-    }
+export const supabase = createClient(supabaseUrl, supabaseServiceRoleKey, {
+  auth: {
+    autoRefreshToken: false,
+    persistSession: false
   }
-);
+});
